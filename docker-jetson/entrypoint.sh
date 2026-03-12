ba7d7370539772b5d7225e16f6d3514f123b3f45
Hayden Mai <144507076+haydenmai@users.noreply.github.com>
2026-03-12 16:21:14 -0700

    Updated jetson ros stuff again

--- docker-jetson/entrypoint.sh
+++ docker-jetson/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-source $ROS_ROOT/install/setup.bash
+if [ -n "${ROS_ROOT:-}" ] && [ -f "$ROS_ROOT/install/setup.bash" ]; then
+	source "$ROS_ROOT/install/setup.bash"
+elif [ -f "/opt/ros/${ROS_DISTRO:-humble}/setup.bash" ]; then
+	source "/opt/ros/${ROS_DISTRO:-humble}/setup.bash"
+fi
 source ~/ros2_ws/install/setup.bash 2>/dev/null || true
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export ROS_DOMAIN_ID=0
